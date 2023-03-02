23b59ec9376bbd1c7d1f65f0f200bba1ee96192b
hjh1161514 <hjh1161514@naver.com>
2023-03-02 15:48:33 +0900

    [FEAT] #3 - 음식 아이템 뷰를 ListView로 구현

--- food_recipe/lib/main.dart
+++ food_recipe/lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  HttpOverrides.global = NoCheckCertificateHttpOverrides(); // 생성된 HttpOverrides 객체 등록
+  HttpOverrides.global =
+      NoCheckCertificateHttpOverrides(); // 생성된 HttpOverrides 객체 등록
   runApp(MyApp());
 }
 
@@ -29,27 +30,27 @@ class HomePage extends StatelessWidget {
     List<Map<String, dynamic>> dataList = [
       {
         "category": "수제버거",
-        "imgUrl": "https://i.ibb.co/HBGKYn4/foodiesfeed-com-summer-juicy-beef-burger.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg",
       },
       {
         "category": "건강식",
-        "imgUrl": "https://i.ibb.co/mB5YNs2/foodiesfeed-com-pumpkin-soup-with-pumpkin-seeds-on-top.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2022/12/09/02/13/korean-7644379_960_720.jpg",
       },
       {
         "category": "한식",
-        "imgUrl": "https://i.ibb.co/Kzzpc97/Beautiful-vibrant-shot-of-traiditonal-Korean-meals.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2017/08/08/09/44/food-photography-2610863_960_720.jpg",
       },
       {
         "category": "디저트",
-        "imgUrl": "https://i.ibb.co/DL5vJVZ/foodiesfeed-com-carefully-putting-a-blackberry-on-tiramisu.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2018/05/01/18/21/eclair-3366430_960_720.jpg",
       },
       {
         "category": "피자",
-        "imgUrl": "https://i.ibb.co/qsm8QH4/pizza.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2017/12/10/14/47/pizza-3010062_960_720.jpg",
       },
       {
         "category": "볶음밥",
-        "imgUrl": "https://i.ibb.co/yQDkq2X/foodiesfeed-com-hot-shakshuka-in-a-cast-iron-pan.jpg",
+        "imgUrl": "https://cdn.pixabay.com/photo/2016/10/23/09/37/fried-rice-1762493_960_720.jpg",
       },
     ];
 
@@ -98,32 +99,40 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Image.network(
-                    "https://cdn.pixabay.com/photo/2016/11/18/15/03/burger-1835192_960_720.jpg",
-                    height: 120,
-                    width: double.infinity,
-                    fit: BoxFit.fill
+
+          Divider(height: 1),
+
+          Expanded(child: ListView.builder(
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.network(
+                          dataList[index]["imgUrl"],
+                          height: 120,
+                          width: double.infinity,
+                          fit: BoxFit.fill
+                      ),
+                      Container(
+                          height: 120,
+                          width: double.infinity,
+                          color: Colors.black.withOpacity(0.5)
+                      ),
+                      Text(
+                        dataList[index]["category"],
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      )
+                    ]
                 ),
-                Container(
-                    height: 120,
-                    width: double.infinity,
-                    color: Colors.black.withOpacity(0.5)
-                ),
-                Text(
-                    "수제버거",
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                    ),
-                )
-            ]
-            ),
-          ),
+              );
+            },
+          ))
         ],
       ),
     );
